e83edaefcc56b8165313015c49bf69119d627d9a
Diego Diniz <diegodinizz@gmail.com>
2020-07-09 18:07:16 +0100

    SignUp component added

--- src/pages/SigninAndSignupPage.js
+++ src/pages/SigninAndSignupPage.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import styled from 'styled-components'
+
 import { SignIn } from '../components/SignIn'
+import { SignUp } from '../components/SignUp'
+
+const Container = styled.div`
+  width: 850px;
+  display: flex;
+  justify-content: space-between;
+  margin: 30px auto;
+`
 
 export const SignInAndSignUpPage = () => (
-  <div>
+  <Container>
     <SignIn />
-  </div>
+    <SignUp />
+  </Container>
 )
