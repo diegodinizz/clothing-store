e39d50815a31c5ada624a56e8c107c28c0d381d5
Diego Diniz <diegodinizz@gmail.com>
2020-08-11 12:58:22 +0100

    created a new selectors to map CollectionsPreview

--- src/components/CollectionsOverview.js
+++ src/components/CollectionsOverview.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 
 import { CollectionPreview } from './CollectionPreview'
-import { selectCollections } from '../redux/shop/shop.selectors'
+import { selectCollectionsForPreview } from '../redux/shop/shop.selectors'
 
 const Container = styled.div`
   display: flex;
@@ -20,7 +20,7 @@ const CollectionsOverview = ({ collections }) => (
 )
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
+  collections: selectCollectionsForPreview
 })
 
 export default connect(mapStateToProps)(CollectionsOverview)
